32c61fc811e2509c7b51baa84f546d9ba3d7370c
macbook <macbook@macbooks-MacBook-Pro.local>
2024-09-23 13:01:35 +0700

    update6

--- app/controllers/lineChatBot.controller.js
+++ app/controllers/lineChatBot.controller.js
@@ -11,7 +11,7 @@ const axios = require("axios");
 // API CHAT BOT
 const line = require("@line/bot-sdk");
 // check channel access token + channel Secret
-
+const channelAccessToken = process.env.LINE_CHANNEL_ACCESS_TOKEN;
 // const config = {
 //   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
 //   channelSecret: process.env.channelSecret,
